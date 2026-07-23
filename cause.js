6e9ec4000fde0426f63d73153e02d2fea56b163f
nrana0692-dev <nrana0692@gmail.com>
2026-07-23 19:04:47 +0530

    Update cause.js

--- cause.js
+++ cause.js
@@ -1,24 +1,24 @@
  // Reasons database
  const reasons = [
     { 
-        text: "You’re such a kind and wonderful person, and I feel lucky to share such a good bond with you. 💖", 
+        text: "You’re such a kind & wonderful person, and I feel lucky to share such a good bond with you", 
         emoji: "🌟",
-        gif: "gif1.gif"
+       
     },
     { 
-        text: "May your day be filled with love, laughter, and endless joy. 🌸 ", 
+        text: "May your life be filled with love, laughter, and endless joy 👻", 
         emoji: "💗",
-        gif: "gif2.gif"
+        
     },
     { 
-        text: "Wishing you success, happiness, and everything your heart desires. ✨ ", 
+        text: "Wishing you success, happiness and everything your heart desires ✨ ", 
         emoji: "💕",
-        gif: "gif1.gif"
+        
     },
     { 
         text: "Stay the amazing girl you are—always spreading positivity around. Have the happiest year ahead! 🥳 ", 
         emoji: "🌟",
-        gif: "gif2.gif"
+        
     }
 ];
 
